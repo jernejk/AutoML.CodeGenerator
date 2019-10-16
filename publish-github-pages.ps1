13a814c533afed6349f94a3a9cd6970c78f0cd23
Jernej Kavka (JK) <jernejkavka@ssw.com.au>
2019-10-17 00:46:22 +1100

    Added publish to GitHub pages script.

---
+++ publish-github-pages.ps1
@@ -0,0 +1,14 @@
+$pathToSolution = "./src/"
+$projectName = "AutoML.CodeGenerator.Blazor"
+$repoName = "AutoML.CodeGenerator"
+
+Write-Output "----==== Publish $pathToSolution"
+dotnet publish $pathToSolution -c Release -o ./dist/
+Write-Output ""
+
+Write-Output "----==== Copy from ./dist/$projectName/dist"
+Copy-Item -Path "./dist/$projectName/dist/*" -Destination "./" -Recurse -Force
+Write-Output ""
+
+Write-Output "----==== Delete dist folder"
+Remove-Item ./dist/ -Recurse
